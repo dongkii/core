6cf36f48b2cb777eb3d23b65fd6b93ccbb4e0ad3
dongkii <31609765+dongkii@users.noreply.github.com>
2022-12-15 18:02:38 +0900

    관심사의 분리

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepositry;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepositry());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(
+                new MemoryMemberRepositry(),
+                new FixDiscountPolicy());
+    }
+}
